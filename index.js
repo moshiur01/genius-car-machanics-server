03016cc7d84f37b919452b962c9491aa6728c81a
moshiur01 <mr.moshiur314@gmail.com>
2021-10-29 11:13:47 +0600

    port env added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // declare port
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bqmue.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 //
